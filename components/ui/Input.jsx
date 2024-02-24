870f7602cfd0c6bc871c28409d600a5f5afb8a4e
Larbi-Fer <ferhaoui.20044@gmail.com>
2024-02-24 12:51:02 +0100

    Add new style for input

--- components/ui/Input.jsx
+++ components/ui/Input.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const Input = ({ text, round, fullWidth, ...props }) => {
+const Input = ({ text, round, fullWidth, variant, ...props }) => {
     var cls = ''
     if (round) cls += ' round'
     if (fullWidth) cls += ' fullWidth'
     return (
         <div>
             <lable>{text}</lable>
-            <input type="text" {...props} className={'inp ' + cls} />
+            <input type="text" {...props} className={'inp ' + cls + ' ' + variant} />
         </div>
     )
 }
