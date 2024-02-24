0cf821540fff5dbce8aeb4b547832ab8871b26f7
Larbi-Fer <ferhaoui.20044@gmail.com>
2024-02-24 17:24:14 +0100

    CONNECT to mySQL

---
+++ utils/database.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql'
+
+const db = mysql.createConnection({
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.DATABASE_DB
+})
+
+db.connect(err => {
+    console.log("Connect to mysql\n", err)
+})
+
+export default db
