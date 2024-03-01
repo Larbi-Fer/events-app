01aa2950524a3ea84de3b6e14d26e362093232b3
Larbi-Fer <ferhaoui.20044@gmail.com>
2024-03-01 12:21:25 +0100

    ADD navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "@styles/globals.css";
+import "@styles/navbar.css";
 import Provider from "@components/shared/Provider";
+import Navbar from "@components/shared/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,12 @@ export default function RootLayout({ children }) {
   return (
     <Provider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navbar />
+          <section>
+            {children}
+          </section>
+        </body>
       </html>
     </Provider>
   );
---
+++ components/shared/Navbar.jsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { useSession } from "next-auth/react"
+import { useRef } from "react"
+
+import Link from "next/link"
+
+import Button from "@components/ui/Button"
+
+const Navbar = () => {
+  const session = useSession()
+  const ref = useRef()
+
+  const handleClick = () => {
+    // show the navigation list in phone view
+    ref.current.classList.toggle('active')
+  }
+
+  return (
+    <header>
+      <h2 className="logo">Events</h2>
+      <nav ref={ref}>
+        { !session.data ?
+        <ul className="nav-links">
+          <li><Link href='/home'>Home</Link></li>
+          <li><Link href='#'>Create New Event</Link></li>
+          <li><Link href='#'>My events</Link></li>
+        </ul>
+        : null}
+      </nav>
+      <div className="buttons">
+        { !session.data ?
+            <>
+              <input type="text" placeholder="Search ..." />
+              <Button>Profile</Button>
+            </>
+          :
+            <>
+              <Link href='/signup' className="cta"><Button round>GET STARTED</Button></Link>
+              <Link href='/login' className="cta"><Button round>LOGIN</Button></Link>
+            </>
+        }
+        <div className="show-items" onClick={handleClick}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
