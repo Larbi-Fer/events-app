6f633c973bd6fc291295eccf90b761b06a783d07
Larbi-Fer <ferhaoui.20044@gmail.com>
2024-03-07 21:36:13 +0100

    ADD get event detail

--- app/api/event/route.js
+++ app/api/event/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 
+import CODES from '@utils/responses'
 import db from '@utils/database'
 
 // Create a new event
@@ -31,6 +32,27 @@ export const POST = async req => {
         })
 
         return NextResponse.json({ success: true, id: result.insertId })
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ success: false })
+    }
+}
+
+
+export const GET = async req => {
+    try {
+        // const body = await req.json()
+        const id = req.nextUrl.searchParams.get('id')
+
+        const result = await new Promise((resolve, reject) => {
+            db.query('SELECT events.*, users.username FROM events, users WHERE events.id=? AND events.creator = users.id', [id], (err, res) => {
+                if (err) return reject(err)
+                resolve(res)
+            })
+        })
+
+        if (!result.length) return NextResponse.json({ success: false, message: CODES.NOT_FOUND })
+        return NextResponse.json({ success: true, event: result[0] })
     } catch (error) {
         console.log(error)
         return NextResponse.json({ success: false })
