70d6eacc7d928ecc3b065507ee389c3f6acea454
Larbi-Fer <ferhaoui.20044@gmail.com>
2024-03-19 07:11:20 +0100

    .

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "@styles/globals.css";
+import "@styles/animations.css";
 import "@styles/navbar.css";
 import Provider from "@components/shared/Provider";
 import Navbar from "@components/shared/Navbar";
