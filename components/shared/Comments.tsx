f83fde6da29cc507b82433f617f77ab42304e6f3
Larbi-Fer <ferhaoui.20044@gmail.com>
2024-03-19 07:22:15 +0100

    Comment on the event and delete it

--- components/shared/Comments.tsx
+++ components/shared/Comments.tsx
@@ -1,20 +1,45 @@
 'use client'
 
 import '@styles/comments.css'
-import { getComments } from "@utils/api"
+import { deleteComment as deleteCommentById, getComments, insertComment } from "@utils/api"
 import { useEffect, useState } from "react"
 import { CommentsLimit } from '@utils/const'
 import { fromNow } from '@utils'
-import authorIcon from '@mui/icons-material/Create';
+import AuthorIcon from '@mui/icons-material/Create';
+import ReplyIcon from '@mui/icons-material/Reply';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Input from '@components/ui/Input'
+import { Moment } from 'moment'
+import Button from '@components/ui/Button'
+import Message from '@components/ui/Message'
+import Loading from '@components/ui/Loading'
 
 type CommentsProps = {
   eventId: number,
-  eventCreatorId: number | null,
-  fullWidth: boolean,
+  eventCreatorId: number,
+  user?: any,
+  fullWidth?: boolean,
 }
 
-const Comments = ({ eventId, eventCreatorId, fullWidth } : CommentsProps) => {
-  const [comments, setComments] = useState<any[]>()
+type CommentProps = {
+  id?: number,
+  authorId: number,
+  eventId: number,
+  text: string,
+  date?: string | Date | Moment,
+
+  author?: string,
+  image?: string,
+  replies?: CommentProps[],
+  reply?: number,
+  isReplyButton?: boolean
+}
+
+const Comments = ({ eventId, eventCreatorId, user, fullWidth } : CommentsProps) => {
+  const [comments, setComments] = useState<CommentProps[]>()
+  const [loading, setLoading] = useState<[boolean, number]>([false, -1])
+  const [deleteCom, setDeleteCom] = useState<[number, boolean]>(null)
 
   useEffect(() => {
     const getData = async() => {
@@ -25,57 +50,140 @@ const Comments = ({ eventId, eventCreatorId, fullWidth } : CommentsProps) => {
     getData()
   }, [eventId])
   
+  const addComment = (type: 'comment' | 'reply', id?: number, i=-1) => async(e: any) => {
+    e.preventDefault()
+
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+    setLoading([true, i])
+
+    const comment = e.target.elements['id-comment'].value
+
+    const commentData: CommentProps = { authorId: user.id, eventId, text: comment }
+    if (type == 'reply') commentData.reply = id
+    const data = await insertComment(commentData)
+    const insertData = { ...commentData, id: data.insertId, author: user.username, date: new Date(), replies: [], image: user.image }
+
+    setComments(old => {
+      if (type === 'comment') return [insertData, ...old];
+
+      return old.map(comment => {
+        if (comment.id === id) {
+          // Add the new reply to the replies array of the corresponding comment
+          return {
+            ...comment,
+            replies: [...comment.replies, { ...insertData, authorId: user.id }],
+          };
+        }
+
+        return comment;
+      });
+    });
+
+    e.target.elements['id-comment'].value = ''
+    setLoading([false, -1])
+  }
+
+  const deleteComment = async() => {
+    const id = deleteCom[0]
+    const isReply = deleteCom[1]
+
+    setDeleteCom([id, null])
+    await deleteCommentById(id)
+
+    setDeleteCom(null)
+    console.log(id, isReply);
+    
+    setComments(prev =>
+      isReply ? prev.map( v => ({ ...v, replies: v.replies.filter( v => v.id != id ) }) ) : prev.filter( v => v.id != id)
+    )
+  }
 
   return (
-    comments ?
-      <div className={"comments" + ( fullWidth ? ' fullWidth' : '' )}>
+    <div className={"comments" + ( fullWidth ? ' fullWidth' : '' )}>
 
         <div className="title">Comments</div>
 
+        <form onSubmit={addComment('comment')} className="comment-inp">
+          <CommentField loading={[...loading, -1]} disabled={!user} id='comment-field' type='Comment' />
+        </form>
+
         {
-          comments.map(comment => 
-            <div className={"comment" + (comment.replies.length ? ' with-replies' : '')}>
-              <div className="comment-header">
-                <div className="image">
-                  <img src="/profile.jpg" alt="Profile logo" />
-                </div>
-                <div className="info">
-                  <div className={"name" + ( comment.authorId == eventCreatorId ? ' author' : '' )}>{comment.author}</div>
-                  <div className="date">{fromNow(comment.date)}</div>
-                </div>
-              </div>
-              <div className="content">
-                <p> {comment.text} </p>
-              </div>
+        comments ?
+          (comments.length ?
+            <>
+              {comments.map((comment, i) =>
+                  <div className={"comment" + (comment.replies.length ? ' with-replies' : '') + ( deleteCom?.[0] == comment.id ? ' disabled' : '' )}>
+                    <Comment comment={comment} author={comment.authorId == eventCreatorId} isLogin={user} isUser={comment.authorId == user?.id} onDelete={id => setDeleteCom([id, false])} reply={() => setComments( prev => prev.map(v => v.id == comment.id ? { ...v, isReplyButton: !v.isReplyButton } : v) )} />
 
-              {/* Replies */}
-              <div className="replies">
-                { comment.replies.map(reply =>
-                  <div className="reply">
+                    <form className='reply-form' onSubmit={addComment('reply', comment.id, i)}>
+                      {comment.isReplyButton ? <CommentField loading={[...loading, i]} disabled={false} id={'reply-' + comment.id} type='Reply' /> : null}
+                    </form>
 
-                    <div className="comment-header">
-                      <div className="image">
-                        <img src="/profile.jpg" alt="Profile logo" />
-                      </div>
-                      <div className="info">
-                        <div className={"name" + ( reply.authorId == eventCreatorId ? ' author' : '' )}>{reply.author}</div>
-                        <div className="date">{fromNow(reply.date)}</div>
-                      </div>
-                    </div>
-                    <div className="content">
-                      <p>
-                        {reply.text}
-                      </p>
+                    {/* Replies */}
+                    <div className="replies">
+                      { comment.replies.map(reply =>
+                        <div className={"reply" + ( deleteCom?.[0] == reply.id ? ' disabled' : '' )}>
+                          <Comment comment={reply} author={reply.authorId == eventCreatorId} isLogin={user} isUser={reply.authorId == user?.id} onDelete={id => setDeleteCom([id, true])} />
+                        </div>
+                      ) }
                     </div>
                   </div>
-                ) }
-              </div>
-            </div>
-        
-          )
+              )}
+              <Message title='You are about to delete your comment' show={Boolean(deleteCom) && deleteCom[1] !== null} onClose={() => setDeleteCom(null)} buttons={[{ text: 'DELETE', onClick: deleteComment }]} width='md'>
+                Are you sure you want to delete this comment.<br />
+                {(() => { const len = comments.find(c => c.id == deleteCom?.[0])?.replies.length; return len ? `You have ${len} repl${ len > 1 ? 'ies' : 'y' }.` : '' })()}
+              </Message>
+            </>
+          :
+            <div>No comment</div>
+          ) : <Loading />
         }
       </div>
-    : <h1>Loading ...</h1>
+  )
+}
+
+const CommentField = ({ disabled, id, type, loading } : { disabled: boolean, id: string, type: 'Comment' | 'Reply', loading: [boolean, number, number] }) => {
+  const load = loading[0] && loading[1] == loading[2]
+  return <Input loading={load} disabled={disabled || load} id={id} className='fade' placeholder={type + ' ...'} name='comment' IconAsButton={<SendIcon fontSize='small' onClick={() => console.log("Clicked !!")} />} variant='fill' round required fullWidth />
+}
+
+type CommentDesignProps = {
+  comment: CommentProps,
+  author: boolean,
+  isUser: boolean,
+  onDelete: (id: number) => any,
+  isLogin: boolean,
+  reply?: () => any,
+}
+
+const Comment = ({ comment, author, isUser, onDelete, reply, isLogin } : CommentDesignProps) => {
+
+  return (
+    <>
+      <div className="options">
+        { (reply && isLogin) && <div className="reply-icon" onClick={reply}>
+          <ReplyIcon />
+        </div> }
+        { isUser && <Button onClick={() => onDelete(comment.id)} circulaire variant='danger' type='color'><DeleteIcon fontSize='small' /></Button> }
+      </div>
+      <div className="comment-header">
+        <div className="image">
+          <img src={comment.image} alt="Profile logo" />
+        </div>
+        <div className="info">
+          <div className={"name" + ( author ? ' author' : '' )}>
+            {comment.author}
+            {author ? <AuthorIcon className='author-icon' fontSize='small' /> : null}
+          </div>
+
+          <div className="date">{fromNow(comment.date)}</div>
+        </div>
+      </div>
+      <div className="content">
+        <p> {comment.text} </p>
+      </div>
+    </>
   )
 }
 
