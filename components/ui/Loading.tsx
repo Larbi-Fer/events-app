04ce35e34f053581c6df55e52a6cbf74c3b8f4fb
Larbi-Fer <ferhaoui.20044@gmail.com>
2024-03-19 07:09:08 +0100

    ADD loading effect

---
+++ components/ui/Loading.tsx
@@ -0,0 +1,12 @@
+import '@styles/loading.css'
+
+const Loading = () => {
+  return (
+    <div className="loading-spinner">
+        <div className="ring"></div>
+        <span>Loading...</span>
+    </div>
+  )
+}
+
+export default Loading
