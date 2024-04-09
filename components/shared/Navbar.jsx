a1ff533eaed648b17895ab85e7553b2c27142dc4
Larbi-Fer <ferhaoui.20044@gmail.com>
2024-04-09 16:01:09 +0100

    ADD notifications box

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,52 +1,89 @@
 'use client'
 
 import { useSession } from "next-auth/react"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 import Link from "next/link"
 
 import Button from "@components/ui/Button"
+import Notifications from "@components/shared/Notifications"
+import NotificationsIcon from "@mui/icons-material/Notifications"
+import { getNumOfNotifications, readNotifications } from "@utils/api"
 
 const Navbar = () => {
   const session = useSession()
-  const ref = useRef()
+  const [showNotif, setShowNotif] = useState(false)
+  const [numNotif, setNumNotif] = useState(0)
+
+  const navRef = useRef()
 
   const handleClick = () => {
     // show the navigation list in phone view
-    ref.current.classList.toggle('active')
+    navRef.current.classList.toggle('active')
+  }
+
+  useEffect(() => {
+    if (!session.data) return
+    // Get number of new notifications
+    const getNumber = async() => {
+      const data = await getNumOfNotifications(session.data.user.id)
+      if (!data.success) return
+      setNumNotif(data.number)
+    }
+    getNumber()
+  }, [session])
+  
+  const openNotifications = async() => {
+    setShowNotif(prev => !prev)
+
+    if (!numNotif) return
+    // Read the notifications
+    setNumNotif(0)
+    await readNotifications(session.data.user.id)
   }
 
   return (
-    <header className="fade">
-      <h2 className="logo pan">Events</h2>
-      <nav ref={ref}>
-        { session.data ?
-        <ul className="nav-links">
-          <li  className="pan"><Link href='/home'>Home</Link></li>
-          <li className="pan d1"><Link href='/create'>Create New Event</Link></li>
-          <li className="pan d2"><Link href={'/profile/' + session.data?.user.id}>My Profile</Link></li>
-        </ul>
-        : null}
-      </nav>
-      <div className="buttons">
-        { session.data ?
-            <>
-              <input className="pan d3" type="text" placeholder="Search ..." />
-              <Button className="pan d4" round>{session.data.user.username}</Button>
-            </>
-          :
-            <>
-              <Link href='/signup' className="cta pan"><Button round>GET STARTED</Button></Link>
-              <Link href='/login' className="cta pan d1"><Button round>LOGIN</Button></Link>
-            </>
-        }
-        <div className="show-items" onClick={handleClick}>
-          <span></span>
-          <span></span>
-          <span></span>
+    <>
+      <div className={showNotif ? 'back-box' : ''} onClick={() => setShowNotif(false)}></div>
+      <header className="fade">
+        <h2 className="logo pan">Events</h2>
+        <nav ref={navRef}>
+          { session.data ?
+          <ul className="nav-links">
+            <li  className="pan"><Link href='/home'>Home</Link></li>
+            <li className="pan d1"><Link href='/create'>Create New Event</Link></li>
+            <li className="pan d2"><Link href={'/profile/' + session.data?.user.id}>My Profile</Link></li>
+          </ul>
+          : null}
+        </nav>
+        <div className="buttons">
+          { session.data ?
+              <>
+                <input className="pan d3" type="text" placeholder="Search ..." />
+                <Button className="pan d4" round>{session.data.user.username}</Button>
+              </>
+            :
+              <>
+                <Link href='/signup' className="cta pan"><Button round>GET STARTED</Button></Link>
+                <Link href='/login' className="cta pan d1"><Button round>LOGIN</Button></Link>
+              </>
+          }
+          <div className="show-items" onClick={handleClick}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          { session.data &&
+            <div className="">
+              <div className={"notifications-button pan d5" + (numNotif ? ' num' : '')} onClick={openNotifications} num={numNotif}>
+                <NotificationsIcon />
+              </div>
+            </div>
+          }
         </div>
-      </div>
-    </header>
+      </header>
+      {session.data && <Notifications show={showNotif} id={session.data.user.id} close={() => setShowNotif(false)} />}
+    </>
   )
 }
 
---
+++ components/shared/Notifications.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import { useEffect, useRef, useState } from "react"
+
+import Link from "next/link"
+
+import { fromNow } from "@utils"
+import { getNotifications, openANotification } from "@utils/api"
+
+type NotifProps = {
+  id: number,
+  logo: string,
+  message: string,
+  createdAt: string,
+  redirectUrl: string,
+  isRead: boolean,
+  isOpen: boolean
+}[]
+
+const Notifications = ({ id, show, close } : { id: number, show: boolean, close: () => void }) => {
+  const [notif, setNotif] = useState<NotifProps>()
+  const ref = useRef<HTMLDivElement>(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    // Open and close notifications box
+    if(show) {
+      ref.current.classList.remove('inactive')
+      ref.current.classList.add('active')
+    } else {
+      ref.current.classList.remove('active')
+      ref.current.classList.add('inactive')
+    }
+
+    if (!show || notif) return
+    // On opened notifications box in first time, get the notifications
+    const getNotif = async () => {
+      const data = await getNotifications(id)
+
+      if (!data.success) return
+      setNotif(data.notifications)
+    }
+    getNotif()
+  }, [show])
+  
+  const goToUrl = (url: string, notifId: number) => async e => {
+    e.preventDefault()
+
+    if (notif.find(n => n.id == notifId).isOpen) {
+      close()
+      return router.push(url)
+    }
+    // Update isOpen field to true
+    openANotification(id, notifId)
+    setNotif(prev => prev.map(n => n.id == notifId ? {...n, isOpen: true} : n))
+    router.push(url)
+    close()
+  }
+
+  return (
+    <div className="notifications" ref={ref}>
+      <h1>Notifications</h1>
+      <div>
+        {notif?.map(n => (
+          <Link href={n.redirectUrl} key={n.id} className={"notif c2" + (n.isOpen ? '' : ' new')} onClick={goToUrl(n.redirectUrl, n.id)}>
+            <div>
+              <img src={n.logo} alt="logo" className="icon" />
+            </div>
+            <div>
+              <p>{n.message}</p>
+              <p>{fromNow(new Date(n.createdAt))}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Notifications
